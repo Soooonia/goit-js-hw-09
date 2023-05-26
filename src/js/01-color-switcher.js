5fe65d6d9c4858f0d723301cfbf215ff240a4f03
Soooonia <121182239+Soooonia@users.noreply.github.com>
2023-05-26 11:32:11 +0300

    Ex 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+  const startBtn = document.querySelector('[data-start]');
+  const closeBtn = document.querySelector('[data-stop]');
+  const body = document.querySelector('body');
+  const COLOR_CHANGE_TIME = 1000;
+let intervalID = null;
+
+console.log(body)
+
+startBtn.addEventListener('click', onStart);
+closeBtn.addEventListener('click', onStop);
+
+
+function onStart(e){
+    body.style.backgroundColor = getRandomHexColor;
+    startBtn.disabled = true;
+    closeBtn.disable = false
+
+ 
+intervalID = setInterval(()=>{
+    body.style.backgroundColor=getRandomHexColor(); 
+},COLOR_CHANGE_TIME) 
+}
+function onStop() {
+    clearInterval(intervalID);
+    startBtn.disabled = false;
+    closeBtn.disabled = true;
+}
